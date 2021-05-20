ecf8948c0befad67ad721f5c8fdae731bcec635a
Dana Alami <dana.alami18@gmail.com>
2021-05-20 13:55:38 +0300

    created the tables of role,department and employees

--- trackemployees.sql
+++ trackemployees.sql
@@ -1,3 +1,28 @@
 DROP DATABSE IF EXISTS employee_trackerDB;
 CREATE DATABSE employee_trackerDB;
 USE employee_trackerDB;
+
+CREATE TABLE department (
+    id INT AUTO_INCEREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCEREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(),
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCEREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT NULL
+    PRIMARY KEY (id)
+);
