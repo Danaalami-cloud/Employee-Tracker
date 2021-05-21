e3d39f0a3f84c4efaf020866f7574e47774e39cf
Dana Alami <dana.alami18@gmail.com>
2021-05-21 16:36:42 +0300

    Updated initial function on application start

--- server.js
+++ server.js
@@ -2,55 +2,50 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'donthackmebish25!',
-    database: 'employee_trackerDB',
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "donthackmebish25!",
+  database: "employee_trackerDB",
 });
 
 connection.connect((err) => {
-    if (err) throw err;
-    console.log(`connected as id ${connection.threadId}`);
-  connection.end();
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+  initialiseQuestions();
 });
 
-const questions = () => {
-return inquirer.prompt([
-    {
-        name: 'action',
-        type: 'list',
-        message: 'Hiya!Welcome to our databse, what would you like to do?',
-        choices: ['manage roles,department, and employees', 'view department', 'view roles', 'view employees', 'delete all of them', 'update role of emplyees' ,'exit app']
-    }
-])
-.then((answer)) => {
-switch (answer.action){
-    case 'manage roles, department,and employees':
-        manageAll();
-    case 'view departments':
-        viewDepartments();
-    case 'view roles':
-            viewRoles();
-    case 'view employees':
-            viewEmployeess();
-            case 'delete all of them':
-                deleteAll();
-                case 'update role of employees':
-                    updateEmployees();
-                    case 'Exit app':
-                        exitApp();
-
+function initialiseQuestions() {
+  inquirer
+    .prompt([
+      {
+        name: "action",
+        type: "list",
+        message: "Hiya!Welcome to our databse, what would you like to do?",
+        choices: [
+          "view departments",
+          "view roles",
+          "view employees",
+          "Add employee",
+          "add department",
+          "add role",
+          "update role of emplyees",
+          "exit app",
+        ],
+      },
+    ])
+    .then((action) => {
+      console.log(action);
+    });
 }
-}
-};
 
+/*
 function manageAll() {
     const query = 'SELECT * FROM'
     inquirer.prompt({
-        type:"input";
-        name: "All";
-        message: "what do you want to manage?";
+        type:"input",
+        name: "All",
+        message: "what do you want to manage?",
         choices: "role","departments", "employees";
     })
 }
@@ -89,3 +84,4 @@ function updateEmployees() {
 function exitApp() {
 
 }
+*/
