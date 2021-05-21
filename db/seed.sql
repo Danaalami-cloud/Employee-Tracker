221fa070d84c857e7abe083b1dd84d50ff81f64c
Dana Alami <dana.alami18@gmail.com>
2021-05-21 16:29:47 +0300

    Set up seed file

---
+++ db/seed.sql
@@ -0,0 +1,20 @@
+use employee_trackerDB; 
+
+INSERT INTO department
+    (name) 
+VALUES 
+    ('HR'), ('Finance'),('Marketing')
+
+INSERT INTO role 
+    (title, salary, department_id)
+VALUES 
+    ('HR Executive', 25000, 1), ('HR Consultant', 35000, 1),('Accountant', 25000, 2), ('Marketing Executive', 25000, 3)
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Dennis', 'Dean', 1, NULL),
+    ('John', 'Jaconbson', 2, 1)
+    ('Ian', 'Isaacson', 2, 1), 
+    ('Aaron', 'Aaaronson', 3, NULL),
+    ('Isobol', 'Issy', 4, NULL)
--- seed.sql
+++
@@ -1,2 +0,0 @@
-is this where i do the insert into fro the table? ffs
-bc im updating the database???
